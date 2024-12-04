238d73f507bb71d2491db1f82726f91c50f6d507
Liam Brophy <liambrophy.design@gmail.com>
2024-12-04 15:57:19 -0500

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee = {
+    name: "Frank Herbert",
+    streetAddress: "123 Arakis Lane",
+  };
+
+  function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return { ...obj, [key]: value };
+
+}
+
+const updatedEmployee = updateEmployeeWithKeyAndValue(employee, "age", 27);
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+
+const desUpdatedEmployee = updateEmployeeWithKeyAndValue(employee, "age", 27);
+
+function deleteFromEmployeeByKey(employee, key){
+    const newEmployee = {...employee };
+    delete newEmployee[key];
+    return newEmployee;
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+
+}
